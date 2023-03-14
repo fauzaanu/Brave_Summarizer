b5022148b9ce2768988f0a3da46a4849d1d42474
Fauzaan Gasim <86226565+fauzaanu@users.noreply.github.com>
2023-03-14 14:28:55 +0500

    first commit

---
+++ app.mjs
@@ -0,0 +1,53 @@
+import puppeteer from "puppeteer";
+import TelegramBot from "node-telegram-bot-api";
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const token = process.env.TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+bot.onText(/\/brave (.+)/, async (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  const photo = await BraveSummarizer(resp);
+
+  if (photo === "Error: Element not found") {
+    bot.sendMessage(chatId, "Brave Summarizer is not found for this query!");
+    return;
+  }
+
+  bot.sendPhoto(chatId, photo);
+});
+
+async function BraveSummarizer(query) {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.goto(`https://search.brave.com/search?q=${query}`, {
+    waituntil: "networkidle0",
+  });
+
+  // we need to screenshot element id of #summarizer if not found then wait for 10 seconds and return error
+  try {
+    await page.waitForSelector("#summarizer", { timeout: 10000 });
+  } catch (err) {
+    await browser.close();
+    return "Error: Element not found";
+  }
+
+  // screenshot the element
+  await page.screenshot({
+    path: "screenshot.png",
+
+    // clip the element
+    clip: await page.$eval("#summarizer", (el) => {
+      const { x, y, width, height } = el.getBoundingClientRect();
+      return { x, y, width, height };
+    }),
+  });
+
+  await browser.close();
+  return "screenshot.png";
+}
