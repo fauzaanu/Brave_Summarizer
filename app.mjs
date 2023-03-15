c20509fa5a6e1b29fdda8cb2cfc2e653b55dca8c
Fauzaan Gasim <86226565+fauzaanu@users.noreply.github.com>
2023-03-15 18:41:26 +0500

    nosandbox

--- app.mjs
+++ app.mjs
@@ -24,7 +24,10 @@ bot.onText(/\/brave (.+)/, async (msg, match) => {
 
 async function BraveSummarizer(query) {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox"],
+    });
     const page = await browser.newPage();
     await page.goto(`https://search.brave.com/search?q=${query}`, {
       waituntil: "networkidle0",
